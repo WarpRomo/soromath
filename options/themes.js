07ff1f1e34a4b9d52e8b428e8001bd63f6042170
WarPromo <kooltyme1@gmail.com>
2023-08-31 03:31:36 -0700

    Add files via upload

--- options/themes.js
+++ options/themes.js
@@ -1,8 +1,5 @@
 var r = document.querySelector(':root');
 
-let currenttheme = localStorage['theme'] || "default";
-let themeselect = null;
-
 
 let themes = {
   "default": {
@@ -15,6 +12,7 @@ let themes = {
     text_select_color: 'rgb(255,0,0)',
     display_color: 'rgb(255,255,255)',
     font_family: "Arial",
+    name: "default",
   },
   "matrix": {
     background: 'rgb(0,0,0)',
@@ -26,6 +24,7 @@ let themes = {
     text_select_color: 'rgb(255,0,0)',
     display_color: 'rgb(0,255,0)',
     font_family: "Monospace",
+    name: "matrix"
   },
   "sakura": {
     background: 'rgb(10,10,15)',
@@ -37,6 +36,7 @@ let themes = {
     text_select_color: 'rgb(255,77,255)',
     display_color: 'rgb(255,77,255)',
     font_family: "Arial",
+    name: "sakura"
   },
   "lime": {
     background: `rgb(${0*0.1},${77*0.05},${0*0.1})`,
@@ -47,7 +47,8 @@ let themes = {
     text_color: 'rgb(255,255,255)',
     text_select_color: 'rgb(0,255,0)',
     display_color: 'rgb(0,255,0)',
-    font_family: "cursive",
+    font_family: "Cursive",
+    name: "lime",
   },
   "ocean": {
     background: 'rgb(10,10,35)',
@@ -59,6 +60,7 @@ let themes = {
     text_select_color: 'rgb(50,50,170)',
     display_color: 'rgb(50,50,170)',
     font_family: "Times New Roman",
+    name: "ocean",
   },
   "fire": {
     background: 'rgb(50,10,10)',
@@ -70,6 +72,7 @@ let themes = {
     text_select_color: 'rgb(170,50,50)',
     display_color: 'rgb(170,50,50)',
     font_family: "Times New Roman",
+    name: "fire"
   },"light": {
       background: 'rgb(255,255,255)',
       button_background: 'rgb(220,220,220)',
@@ -80,6 +83,7 @@ let themes = {
       text_select_color: 'rgb(255,0,0)',
       display_color: 'rgb(0,0,0)',
       font_family: "Monospace",
+      name: "light"
   },
   "shadow": {
       background: 'rgb(0,0,0)',
@@ -91,10 +95,31 @@ let themes = {
       text_select_color: 'rgb(100,100,100)',
       display_color: 'rgb(255,255,255)',
       font_family: "Arial",
+      name: "shadow"
   },
-
-
 }
+
+let currenttheme = themes["default"];
+
+try{
+  if(localStorage.getItem('theme') != null) currenttheme = JSON.parse(localStorage.getItem('theme'));
+}
+catch(err){}
+
+
+let themeselect = null;
+
+
+
+
+let themecustompresets = [];
+try{
+  if(themecustompresets != null) themecustompresets = JSON.parse(localStorage['customthemes']);
+}
+catch(err){}
+
+let themeediting = {};
+let themeeditingindex = -1;
 
 function showthemeselect(){
 
@@ -105,38 +130,242 @@ function showthemeselect(){
 }
 
 
-function changetheme(theme){
+function matchcurrenttheme(){
+
+  let keys = Object.keys(currenttheme);
+
+  let k1 = Object.keys(themes);
+
+  L: for(var i = 0; i < k1.length; i++){
+
+    for(var j = 0; j < keys.length; j++){
+
+      if(themes[k1[i]][keys[j]] != currenttheme[keys[j]]) continue L;
+
+    }
+
+    console.log("FOUND MATCH", themes[k1[i]].name)
+
+    currenttheme = themes[k1[i]];
+    return;
+
+  }
+
+  k1 = Object.keys(themecustompresets);
+
+  L: for(var i = 0; i < k1.length; i++){
+
+    for(var j = 0; j < keys.length; j++){
+
+      if(themecustompresets[k1[i]][keys[j]] != currenttheme[keys[j]]) continue L;
+
+    }
+
+    console.log("FOUND MATCH", themecustompresets[k1[i]].name)
+
+    currenttheme = themecustompresets[k1[i]];
+    return;
+
+  }
+
+
+}
+
+function savethemelocal(){
+  localStorage.setItem('theme', JSON.stringify(currenttheme));
+  localStorage.setItem('customthemes', JSON.stringify(themecustompresets));
+}
+
+function changetheme(theme,hidescreen=true){
+
   currenttheme = theme;
-  localStorage['theme'] = currenttheme;
+
+  localStorage.setItem('theme', JSON.stringify(currenttheme));
+
+  console.log("SET THAT");
+
+  localStorage.setItem('customthemes', JSON.stringify(themecustompresets));
+
   settheme(currenttheme);
 
   console.log("here");
+  console.log(theme);
 
   let button = document.getElementById("themeselectbutton");
-  button.innerHTML = theme;
+  button.innerHTML = theme.name;
 
-  let text_color = themes[theme].display_color;
+  if(hidescreen){
+    let container = document.getElementById("themecontainer");
+    container.style.display = "none";
+  }
 
-  button.style.color = `rgb(` + text_color[0] + ',' + text_color[1] + ',' + text_color[2] + ')'
+}
+
+function themeeditingname(event){
+  themeediting.name = event.target.value;
+
+  syncthemeedit();
+}
+
+function themeeditingfont(event){
+  themeediting.font_family = event.target.value
+
+  settheme(themeediting);
+  syncthemeedit();
+}
+
+function deletethemeediting(){
+
+  let themecustomizecontainer = document.getElementById("themecustomizecontainer");
+  let themecustomize = document.getElementById("themecustomize");
+  let themecustomizebuttext = document.getElementById("themecustomizebuttext");
+  let themesettingsbutton = document.getElementById("themesettingsbutton");
+
+  let themeselect = document.getElementById("themeselect")
+  let index = themecustompresets.indexOf(themeediting);
+  let preset = themeediting;
+  let buttonelem = themeselect.children[index]
+
+  if(currenttheme == themeediting){
+
+    changetheme(themes["default"], false);
+
+  }
+
+  themecustompresets.splice(index, 1);
+  buttonelem.remove();
+
+  let animTime = 300;
+
+  $(themecustomizecontainer).animate({
+
+    opacity: 0
+
+  }, animTime, () => {
+
+    themecustomizecontainer.style.display = "none";
+    themecustomizecontainer.style.opacity = 1;
+
+  })
+
+}
+
+function syncthemeedit(){
 
 
-  let container = document.getElementById("themecontainer");
-  container.style.display = "none";
+  let buttonelem2 = document.getElementById("themesettingsbutton");
+  let themeselect = document.getElementById("themeselect")
+  let index = themecustompresets.indexOf(themeediting);
+  let preset = themeediting;
+  let buttonelem = themeselect.children[index]
+
+  buttonelem.children[0].innerHTML = preset.name;
+  buttonelem.style.background = themeediting.background
+  buttonelem.style.fontFamily = themeediting.font_family
+  buttonelem.style.color = themeediting.text_color
+  buttonelem.style.border = `solid 1px ${themeediting.button_border}`
+
+  buttonelem2.children[0].innerHTML = preset.name;
+  buttonelem2.style.background = themeediting.background
+  buttonelem2.style.fontFamily = themeediting.font_family
+  buttonelem2.style.color = themeediting.text_color
+  buttonelem2.style.border = `solid 1px ${themeediting.button_border}`
+
 }
 
 function themeinit(){
+
+    matchcurrenttheme();
 
     let keys = Object.keys(themes);
 
     let themeselect = document.getElementById("themeselect")
 
+    let themesettingsbutton = document.getElementById("themesettingsbutton");
+
+    let themesettingstext = document.createElement("p");
+
+    themesettingstext.id = "themecustomizebuttext";
+
+    themesettingsbutton.appendChild(themesettingstext);
+
+    let backbutton = document.createElement("button");
+    backbutton.classList.add("zulubutton");
+    backbutton.classList.add("themeplus");
+
+    let backimage = document.createElement("img");
+    backimage.src = "settingsback.png";
+
+    for(var i = 0; i < themecustompresets.length; i++){
+      genpresetbutton(i);
+    }
+
+    backbutton.appendChild(backimage);
+
+    themesettingsbutton.appendChild(backbutton);
+
+
+
+
+    let colorselects = document.getElementsByClassName("themecolorraw");
+
+    let length = colorselects.length;
+
+    let added = [];
+
+    for(var i = 0; i < length; i++){
+
+      let element = colorselects[i];
+      colorselects[i].style.opacity = "1"
+
+      let placeholder = document.createElement("div");
+      placeholder.style.background = colorselects[i].value;
+
+      let key = element.parentElement.parentElement.children[0].innerHTML;
+
+      key = key.toLowerCase().split(" ").join("_");
+
+
+      colorselects[i].oninput = function() {
+
+        placeholder.style.background = element.value;
+        themeediting[key] = element.value;
+
+        settheme(themeediting);
+        syncthemeedit()
+
+
+
+      }
+
+      colorselects[i].parentElement.appendChild(placeholder);
+
+      added.push(placeholder);
+
+    }
+
+    for(var i = 0; i < added.length; i++){
+      added[i].classList.add("themecolor");
+    }
+
+
+
+
+
     for(var i = 0; i < keys.length; i++){
 
       let num = i;
 
+      let buttoncontainer = document.createElement("div");
+      buttoncontainer.classList.add("themebuttoncontainer");
+
       let buttonelem = document.createElement("button");
-      buttonelem.onclick =  () => {
-        changetheme(keys[num])
+
+      buttonelem.onclick =  (event) => {
+
+        if(event.target.src != undefined || event.target.classList.contains("themeplus")) return;
+
+        changetheme(themes[keys[num]])
       };
 
 
@@ -145,29 +374,52 @@ function themeinit(){
 
       buttonelem.id = "themebutton" + keys[i];
 
-      buttonelem.innerHTML = keys[i];
-
-      let text_color = themes[keys[i]].display_color;
-
-      buttonelem.style.color = text_color;
-
       let background = themes[keys[i]].background
 
       buttonelem.style.background = background
 
-      let border = themes[keys[i]].background
+      let border = themes[keys[i]].button_border;
 
       buttonelem.style.border = `solid 1px ` + border
-
       buttonelem.style.fontFamily = themes[keys[i]].font_family
 
-
-
       buttonelem.onmouseover = () => {
+        settheme(themes[keys[num]])
+      }
 
-        settheme(keys[num])
+      let buttontext = document.createElement("p");
+      buttontext.innerHTML = keys[i];
+      let text_color = themes[keys[i]].text_color;
+      buttontext.style.color = text_color
+
+      buttonelem.appendChild(buttontext)
+
+      let clonebutton = document.createElement("button");
+      clonebutton.classList.add("zulubutton");
+      clonebutton.classList.add("themeplus");
+
+      clonebutton.style.background = themes[keys[i]].button_background;
+      clonebutton.style.border = `solid 1px ${themes[keys[i]].button_border}`
+
+
+      let cloneimage = document.createElement("img");
+      cloneimage.src = "plus.png";
+
+      clonebutton.appendChild(cloneimage);
+
+      clonebutton.onclick = () => {
+
+        let newpreset = JSON.parse(JSON.stringify(themes[keys[num]]));
+        newpreset.name = keys[num] + " preset";
+        themecustompresets.push(newpreset);
+
+        let newelem = genpresetbutton(themecustompresets.length-1);
+
 
       }
+
+
+      buttonelem.appendChild(clonebutton);
 
       themeselect.appendChild(buttonelem);
 
@@ -179,29 +431,239 @@ function themeinit(){
 
 }
 
-function removethemefocus(event){
 
-  if(event.target.classList.contains("themebutton")) return;
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function setthemesettings(preset){
+
+  let settings = document.getElementsByClassName("themesetting");
+
+  for(var i = 0; i < settings.length; i++){
+
+    let setting = (settings[i].children[0].innerHTML)
+
+    if(setting == "Preset Name"){
+
+      settings[i].children[1].value = preset.name;
+
+    }
+    else if(setting == "Font Family"){
+      settings[i].children[1].value = preset.font_family;
+    }
+    else{
+      let key = setting.toLowerCase().split(" ").join("_");
+
+      let value = preset[key];
+
+      if(value.includes("rgb")){
+
+        var rgb = value.split( ',' ) ;
+        let r = parseInt( rgb[0].substring(4) ) ; // skip rgb(
+        let g = parseInt( rgb[1] ) ; // this is just g
+        let b = parseInt( rgb[2] ) ; // parseInt scraps trailing )
+
+        value = rgbToHex(r,g,b);
+
+      }
+
+      settings[i].children[1].children[0].value = value;
+      settings[i].children[1].children[1].style.background = value;
+
+    }
+
+
+
+  }
+
+}
+
+
+function genpresetbutton(index){
+
+  let themeselect = document.getElementById("themeselect")
+
+  let buttoncontainer = document.createElement("div");
+  buttoncontainer.classList.add("themebuttoncontainer");
+
+  let buttonelem = document.createElement("button");
+  let preset = themecustompresets[index];
+
+
+  buttonelem.onclick =  (event) => {
+
+    if(event.target.src != undefined || event.target.classList.contains("themeplus")) return;
+
+    changetheme(preset);
+  };
+
+
+
+  buttonelem.classList.add("zulubutton");
+  buttonelem.classList.add("themebutton");
+
+  //buttonelem.id = "themebutton" + keys[i];
+
+  let background = preset.background
+
+  buttonelem.style.background = background
+
+  let border = preset.button_border;
+
+  buttonelem.style.border = `solid 1px ` + border
+  buttonelem.style.fontFamily = preset.font_family
+
+  buttonelem.onmouseover = () => {
+    settheme(preset)
+  }
+
+  let buttontext = document.createElement("p");
+  buttontext.innerHTML = preset.name;
+  let text_color = preset.text_color;
+  buttonelem.style.color = text_color
+
+  buttonelem.appendChild(buttontext)
+
+  let changebutton = document.createElement("button");
+  changebutton.classList.add("zulubutton");
+  changebutton.classList.add("themeplus");
+
+  changebutton.style.background = preset.button_background;
+  changebutton.style.border = `solid 1px ${preset.button_border}`
+
+
+  let changeimage = document.createElement("img");
+  changeimage.src = "settings.png";
+
+  changebutton.appendChild(changeimage);
+
+  changebutton.onclick = () => {
+
+    setthemesettings(preset);
+    themeediting = preset;
+
+    let animTime = 300;
+
+    let themecustomizecontainer = document.getElementById("themecustomizecontainer");
+    let themecustomize = document.getElementById("themecustomize");
+    let themecustomizebuttext = document.getElementById("themecustomizebuttext");
+
+    themecustomizebuttext.innerHTML = preset.name;
+    themecustomize.style.opacity = 0;
+    themecustomizecontainer.style.display = "";
+
+    let themesettingsbutton = document.getElementById("themesettingsbutton");
+
+    themesettingsbutton.style.background = preset.background
+    themesettingsbutton.style.font_family = preset.font_family
+    themesettingsbutton.style.color = preset.text_color
+    themesettingsbutton.style.border = `solid 1px ${preset.button_border}`
+
+    let offset = buttonelem.offsetTop - themeselect.scrollTop;
+
+    console.log(offset);
+
+
+    buttonelem.style.opacity = 0;
+    themesettingsbutton.style.top = offset + "px";
+
+    $(themecustomize).animate({
+
+      opacity: 1
+
+    }, animTime)
+
+    $(themesettingsbutton).animate({
+
+      top: (themecustomizecontainer.offsetHeight - buttonelem.offsetHeight - 7) + "px",
+
+    }, animTime, "easeOutQuad")
+
+    themesettingsbutton.onclick = () => {
+
+      $(themecustomize).animate({
+
+        opacity: 0
+
+      }, animTime)
+
+      $(themesettingsbutton).animate({
+
+        top: offset + "px",
+
+      }, animTime, "easeOutQuad", () => {
+
+
+        buttonelem.style.opacity = "";
+
+        themecustomizecontainer.style.display = "none";
+
+      })
+
+
+
+    }
+
+
+
+  }
+
+  buttonelem.appendChild(changebutton);
+
+  themeselect.insertBefore(buttonelem, themeselect.childNodes[index+1]);
+
+  return buttonelem;
+
+}
+
+
+
+function removethemefocus(event, me){
+
+  console.log(event.target == me);
+
+  if(event.target != me) return;
 
   let container = document.getElementById("themecontainer");
 
   container.style.display = "none";
 
+  let themecustomizecontainer = document.getElementById("themecustomizecontainer");
+  let themesettingsbutton = document.getElementById("themesettingsbutton");
+
+  if(themecustomizecontainer.style.display == ""){
+    themesettingsbutton.click()
+  }
+
   settheme(currenttheme);
+  savethemelocal();
 
 }
 
-function settheme(theme){
+function settheme(obj){
 
 
-  let keys = Object.keys(themes[theme]);
+  let keys = Object.keys(obj);
 
   for(var i = 0; i < keys.length; i++){
 
-    let value = themes[theme][keys[i]];
+    let value = obj[keys[i]];
     r.style.setProperty(`--${keys[i]}`, value)
 
   }
+
+  if(obj == currenttheme){
+
+    document.getElementById("themeselectbutton").innerHTML = currenttheme.name;
+
+  }
+
 
   makechart()
 
